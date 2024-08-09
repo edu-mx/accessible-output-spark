907193ee6a2da6d97e99c349da6db61008d6d75c
edu-mx <diaseduardo139@gmail.com>
2024-08-08 23:36:31 -0300

    aded metod line_is_null to verify header lines.

--- __init__.py
+++ __init__.py
@@ -1,9 +1,11 @@
 # Copyright (C) Eduardo Araújo AKA Edu-MX < diaseduardo139@gmail.com
-
+import re
 import globalPluginHandler
 import api
 import textInfos
 import ui
+
+pattern = r"^\+(\-+\+)+$"
 
 class Text:
     def __init__(self):
@@ -19,12 +21,22 @@ class Text:
         except (RuntimeError, NotImplementedError):
             return None
 
+        def line_is_null(self, line):
+            return re.match(pattern, line) is not None
+
+        def text_to_dict(self, text):
+            # tratamento de string
+            new_text = text.strip()
+
+
+-
+
 
 text = Text()
 class GlobalPlugin(globalPluginHandler.GlobalPlugin):
     def script_Char(self, gesture):
         selected = text.selected_text()
-        ui.message(selected)
+        ui.message(text.text_to_dict(selected))
 
 
     __gestures = {
